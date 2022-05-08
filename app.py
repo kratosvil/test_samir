84eadcab3776e8290dd3ba156019b7da651502c4
Samir Villa <43276540+kratosvil@users.noreply.github.com>
2022-05-08 13:12:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print ("solaaaaaaaaaaaaaa")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! Samir villa'
+
+if __name__ == "__main__":
+    app.run()
